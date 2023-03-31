b4d86d58f0c9d5e60abdc4eb520786258b5f8816
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-31 13:25:47 +0200

    a trigger method was added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,11 +13,19 @@ class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
   late User loggedInUser;
 
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
   void getCurrentUser() async {
     try {
       final user = await _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
+        print(loggedInUser.email);
       }
     } catch (e) {
       print(e);
